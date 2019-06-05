36a7fcb70e134444bb459289f893d0741e958fa9
asac <asac.cool@gmail.com>
2019-06-05 09:42:08 +0300

    bug fixing related to multiple args for swarm phase

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const exportBrowserInteract = {
         module.exports.createWindowInteractionSpace = require("./lib/interactionSpaceImpl/WindowMQInteractionSpace").createInteractionSpace;
     },
     enableReactInteractions: function () {
-        module.exports.createWindowInteractionSpace = require("./lib/interactionSpaceImpl/WindowMQInteractionSpace").createInteractionSpace;
         module.exports.createWindowMQ = require("./lib/interactionSpaceImpl/specificMQImpl/ChildWndMQ").createMQ;
+        module.exports.createWindowInteractionSpace = require("./lib/interactionSpaceImpl/WindowMQInteractionSpace").createInteractionSpace;
     },
     enableWebViewInteractions:function(){
         module.exports.createWindowInteractionSpace = require("./lib/interactionSpaceImpl/WebViewMQInteractionSpace").createInteractionSpace;
         module.exports.createWindowMQ = require("./lib/interactionSpaceImpl/specificMQImpl/ChildWebViewMQ").createMQ;
     },
     enableLocalInteractions: function () {
-        module.exports.createInteractionSpace = require("./lib/interactionSpaceImpl/memoryMQBasedInteractionSpace").createInteractionSpace;
+        module.exports.createInteractionSpace = require("./lib/interactionSpaceImpl/SoundPubSubMQBasedInteractionSpace").createInteractionSpace;
     },
     enableRemoteInteractions: function () {
         module.exports.createRemoteInteractionSpace = require('./lib/interactionSpaceImpl/httpInteractionSpace').createInteractionSpace;
@@ -31,7 +31,7 @@ if (typeof(navigator) !== "undefined") {
 else {
     module.exports = {
         createNodeInteractionSpace: require("./lib/interactionSpaceImpl/folderMQBasedInteractionSpace").createInteractionSpace,
-        createInteractionSpace: require("./lib/interactionSpaceImpl/memoryMQBasedInteractionSpace").createInteractionSpace,
+        createInteractionSpace: require("./lib/interactionSpaceImpl/SoundPubSubMQBasedInteractionSpace").createInteractionSpace,
         createRemoteInteractionSpace: require('./lib/interactionSpaceImpl/httpInteractionSpace').createInteractionSpace
     }
 }
